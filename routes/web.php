5bbcf3808e85a9d10a69ca6936fc4a0409ac0fee
hasan <hasan@brainmatics.com>
2023-03-24 15:54:33 +0700

    okey

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(['register' => false]);
+Route::prefix('admin')->group(function () {
+
+    Route::group(['middleware' => 'auth'], function(){
+
+        //dashboard
+        Route::get('/dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('admin.dashboard.index');
+        //permissions
+        Route::resource('/permission', App\Http\Controllers\Admin\PermissionController::class, ['except' => ['show', 'create', 'edit', 'update', 'delete'] ,'as' => 'admin']);
+
+    });
+
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
