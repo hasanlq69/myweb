67df5dc79cc7400888493f3c71ad94189e146396
Muhammad Hasan <hasan@brainmatics.com>
2023-03-24 07:34:26 +0700

    hasan

---
+++ app/Models/Post.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    /**
+     * guarded
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * image
+     *
+     * @return Attribute
+     */
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => asset('/storage/categories/' . $value),
+        );
+    }
+}
